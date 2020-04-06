a6dd2452585e79625c9cfc75e88a2b3a9bbff2b7
xyrexy <xyounan@hotmail.com>
2020-04-06 20:56:28 +1000

    folowing tutorial

---
+++ unity-learning/Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] public Vector3 startPosition;
+    [SerializeField] private float speed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // transform.position = new Vector3(0,0,0);
+        transform.position = startPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(new Vector3(horizontalInput,0,0) * (speed * Time.deltaTime));
+    }
+}
