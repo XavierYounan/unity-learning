2fc0731f2a65a406f000f33d92e34d7af5bcb7c9
xyrexy <xyounan@hotmail.com>
2020-04-13 19:20:01 +1000

    learning

---
+++ unity-learning/Assets/Scripts/Aim.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aim : MonoBehaviour
+{
+    public Transform sphere;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Calculate the directoon
+        //direction = destination - source (vector)
+        Vector3 directionToFace = sphere.position - transform.position;
+        Debug.DrawRay(transform.position, directionToFace, Color.red);
+        //face towards direction
+        Quaternion targetRotation = Quaternion.LookRotation(directionToFace);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime);
+    }
+}
